6e06b6dc5b5cc52aa4a51a2ba2eddb1c77488486
VladimirKoshovec <koshavladmir@gmail.com>
2023-01-31 17:19:07 +0200

    finish

--- src/FeedbackBoard/FeedbackBoard.jsx
+++ src/FeedbackBoard/FeedbackBoard.jsx
@@ -1,68 +1,54 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
+const FeedbackBoard = () => {
 
-class FeedbackBoard extends Component {
+    const [ votes, setVotes] = useState ({ good: 0, neutral: 0, bad: 0});
 
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
-
-    updStatistics = (e) => {
+    const updStatistics = (e) => {
         const { name } = e.currentTarget;
-        this.setState (prevstate => {
-            return { [name]: prevstate[name] + 1 }
-        });
+        setVotes(prevVotes => ({...prevVotes , [name]: + prevVotes[name] + 1 }));
     };
 
-    countTotalFeedback = () => {
-        const values = Object.values(this.state);
+    const countTotalFeedback = () => {
+        const values = Object.values(votes);
         return values.reduce((acc, item)=>{ return acc + item }, 0); 
     }
 
-    countPositiveFeedbackPercentage = () => {
-        const total = this.countTotalFeedback();
-        const good = this.state.good;
+    const countPositiveFeedbackPercentage = () => {
+        const total = countTotalFeedback();
+        const good =  votes.good;
         const value = Math.round(good/(total/100));
         return total ? value: 0; 
     }
 
+    const controls = Object.keys (votes); 
 
-    render () {
-
-        const controls = Object.keys (this.state);
-        const { good, neutral, bad } = this.state;
-        const countTotalFeedback = this.countTotalFeedback();
-        const countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-
-        return (
-            <>
-                <Section title={ "Please leave feedback" }> 
-                   <FeedbackOptions 
-                       options = { controls }   
-                       onLeaveFeedback= { this.updStatistics }
-                   />
-                </Section>   
+    return (
+        <>
+            <Section title={ "Please leave feedback" }> 
+               <FeedbackOptions 
+                    options = { controls }   
+                    onLeaveFeedback= { updStatistics }
+                />
+            </Section>   
                 
-                <Section title={ "Statistics" }>
-                   { countTotalFeedback > 0 ? 
-                        <Statistics 
-                            good = { good } 
-                            neutral = { neutral }
-                            bad = { bad }
-                            total = { countTotalFeedback }
-                            positivePercentage = { countPositiveFeedbackPercentage }
-                        /> :
-                        <Notification message='There is no feedback'/> }   
-                </Section>
-            </>
-        )
-    }
+            <Section title={ "Statistics" }>
+                { countTotalFeedback() > 0 ? 
+                <Statistics 
+                    good = { votes.good } 
+                    neutral = { votes.neutral }
+                    bad = { votes.bad }
+                    total = { countTotalFeedback() }
+                    positivePercentage = { countPositiveFeedbackPercentage() }
+                /> :
+                <Notification message='There is no feedback'/> }   
+            </Section>
+        </>
+    );
 }
 
 export default FeedbackBoard;
